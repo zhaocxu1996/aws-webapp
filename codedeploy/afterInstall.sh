baf2859b510a79eae4bcc85112fa06f73c9ca3b9
zhaocxu1996 <xu.zhaoc@northeastern.edu>
2020-03-11 17:38:58 -0700

    assignment7-fix9

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cd /home/centos/webapp
+cd /home/ubuntu
 sudo rm -rf logs/*
-sudo chown -R /home/ubuntu/webapp
 sudo chmod +x ROOT.jar
 source /etc/profile.d/envvariable.sh
 kill -9 $(ps -ef|grep java | grep -v grep)
