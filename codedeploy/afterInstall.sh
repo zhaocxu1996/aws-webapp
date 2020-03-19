4cd454216222f97351b76520da61770f45bfbf79
zhaocxu1996 <xu.zhaoc@northeastern.edu>
2020-03-18 19:58:58 -0700

    fix4

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -4,7 +4,6 @@ sudo rm -rf logs/*
 sudo chown -R ubuntu:ubuntu /home/ubuntu/webapp
 source /etc/profile.d/envvariable.sh
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch-config.json -s
-cd target
 sudo chmod +x ROOT.jar
 kill -9 $(ps -ef|grep java | grep -v grep)
 nohup java -jar ROOT.jar > /home/ubuntu/webapp/output.txt 2> /home/ubuntu/webapp/output.txt < /home/ubuntu/webapp/output.txt &
